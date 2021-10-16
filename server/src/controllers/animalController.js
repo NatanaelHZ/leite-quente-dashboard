b517cce9b7d660cd54f8d15f7e960d7ba0790f0f
NatanaelHZ <natanael_henrik28@outlook.com>
2021-10-16 14:06:29 -0300

    Refatoração

--- server/src/controllers/animalController.js
+++ server/src/controllers/animalController.js
@@ -1,68 +1,66 @@
 const { Animal } = require('../models');
 
-module.exports = class AnimalController {
-  static async get(req, res) {
-    try {
-      const animals = await Animal.findAll();
+exports.get = async (req, res) => {
+  try {
+    const animals = await Animal.findAll();
 
-      return res.status(200).json({
-        animals,
-        message: 'get_success'
-      });
-    } catch (error) {
-      return res.status(400).json({
-        error: error.message
-      });
-    }
+    return res.status(200).json({
+      animals,
+      message: 'get_success'
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: error.message
+    });
   }
+};
 
-  static async getById(req, res) {
-    try {
-      const { id } = req.params;
+exports.getById = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-      const animal = await Animal.findByPk(id);
+    const animal = await Animal.findByPk(id);
 
-      res.json({ data: animal });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+    res.json({ data: animal });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
+};
 
-  static async create(req, res) {
-    try {
-      const AnimalCreated = await Animal.create(req.body);
+exports.create = async (req, res) => {
+  try {
+    const AnimalCreated = await Animal.create(req.body);
 
-      res.status(201).json({
-        data: AnimalCreated,
-        message: 'create_success'
-      });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+    res.status(201).json({
+      data: AnimalCreated,
+      message: 'create_success'
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
+};
 
-  static async update(req, res) {
-    try {
-      const { id } = req.params.id;
-      const animal = { ...req.body };
+exports.update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const animal = { ...req.body };
 
-      const updateAnimal = await Animal.update(animal, { where: { id } });
+    const updateAnimal = await Animal.update(animal, { where: { id } });
 
-      res.status(201).json({ data: updateAnimal, message: 'update_success' });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+    res.status(201).json({ data: updateAnimal, message: 'update_success' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
+};
 
-  static async delete(req, res) {
-    try {
-      const { id } = req.params.id;
+exports.delete = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-      const deletedAnimal = await Animal.destroy({ where: { id } });
+    const deletedAnimal = await Animal.destroy({ where: { id } });
 
-      res.json({ data: deletedAnimal, message: 'delete_success' });
-    } catch (error) {
-      res.status(400).json({ error: error.message });
-    }
+    res.status(200).json({ data: deletedAnimal, message: 'delete_success' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 };
